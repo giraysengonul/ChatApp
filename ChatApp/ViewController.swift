05e18560afafe129f301a3c9f981d95fa8e2d0b7
Hakkı Can Şengönül <coder.9734@gmail.com>
2022-12-10 23:20:20 +0300

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by hakkı can şengönül on 10.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
