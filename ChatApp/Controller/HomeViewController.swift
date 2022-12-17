03be49f2af712a94c8124ab68d9b471fc3c7972a
Hakkı Can Şengönül <coder.9734@gmail.com>
2022-12-17 23:23:52 +0300

    UIBArButtonItems Added

--- ChatApp/Controller/HomeViewController.swift
+++ ChatApp/Controller/HomeViewController.swift
@@ -8,17 +8,28 @@
 import UIKit
 import FirebaseAuth
 class HomeViewController: UIViewController {
-     // MARK: - Properties
-     // MARK: - Lifecycle
+    // MARK: - Properties
+    private var messageButton: UIBarButtonItem!
+    private var newMessageButton: UIBarButtonItem!
+    private var container =  Container()
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-       authenticationStatus()
+        authenticationStatus()
         style()
         layout()
     }
 }
- // MARK: - Helpers
+// MARK: - Helpers
 extension HomeViewController{
+    private func configureBarItem(text: String, selector: Selector)-> UIButton{
+        let button = UIButton(type: .system)
+        button.setTitle(text, for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 22)
+        button.addTarget(self, action: selector, for: .touchUpInside)
+        return button
+    }
     private func authenticationStatus(){
         if Auth.auth().currentUser?.uid == nil{
             DispatchQueue.main.async {
@@ -38,7 +49,40 @@ extension HomeViewController{
     }
     private func style(){
         view.backgroundColor = .white
+        messageButton = UIBarButtonItem(customView: configureBarItem(text: "Message", selector: #selector(handleMessageButton)))
+        newMessageButton = UIBarButtonItem(customView: configureBarItem(text: "New Message", selector: #selector(handleNewMessageButton)))
+        self.navigationItem.leftBarButtonItems = [messageButton, newMessageButton]
+        //container
+        configureContainer()
     }
     private func layout(){
+    }
+    private func  configureContainer(){
+        guard let containerView = container.view else{ return }
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(containerView)
+        NSLayoutConstraint.activate([
+            containerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+}
+// MARK: - Selector
+extension HomeViewController{
+    @objc private func handleMessageButton(){
+        
+    }
+    @objc private func handleNewMessageButton(){
+        
+    }
+}
+
+
+class Container: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
     }
 }
